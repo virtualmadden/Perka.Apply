7c0cb7e2f27045dd63de0cb47006972d5e6e652f
Jon Madden <jmadden@incomm.com>
2017-09-06 14:59:54 -0700

    Moving application logic to it's own class.

---
+++ Perka.Apply.Client/Perka.Apply.Client/Actions/ApplicationActions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Perka.Apply.Client.Adapters;
+using Perka.Apply.Client.Models;
+
+namespace Perka.Apply.Client.Actions
+{
+    public interface IApplicationActions
+    {
+        bool Apply();
+    }
+
+    public class ApplicationActions : IApplicationActions
+    {
+        private readonly IFileSystemAdapter _fileSystemAdapter;
+        private readonly IGithubActions _githubActions;
+        private readonly IPerkaActions _perkaActions;
+
+        public ApplicationActions(IGithubActions githubActions = null, IFileSystemAdapter fileSystemAdapter = null, IPerkaActions perkaActions = null)
+        {
+            _githubActions = githubActions ?? new GithubActions();
+            _fileSystemAdapter = fileSystemAdapter ?? new FileSystemAdapter();
+            _perkaActions = perkaActions ?? new PerkaActions();
+        }
+
+        public bool Apply()
+        {
+            try
+            {
+                var result = GetGithubProjects();
+
+                var resumeContents = GetEncryptedResume();
+
+                var applicationRequest = new PerkaApplicationRequest
+                {
+                    FirstName = "Jonathan",
+                    LastName = "Madden",
+                    Email = "virtualmadden@gmail.com",
+                    PositionId = "GENERALIST",
+                    Explanation = "Custom C# Leveraging HTTPClient - https://github.com/virtualmadden/Perka.Apply.Client",
+                    Projects = result.Select(x => $"{x.Name} - {x.Uri}").ToList(),
+                    Source = "Perka.com",
+                    Resume = resumeContents
+                };
+
+                var response = PostApplicationToPerka(applicationRequest);
+
+                if (response.Response.Equals("You did it!"))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private IEnumerable<GithubProjectResponse> GetGithubProjects()
+        {
+            return _githubActions.GetProjects().Result;
+        }
+
+        private string GetEncryptedResume()
+        {
+            return _fileSystemAdapter.GetBase64EncodedResume();
+        }
+
+        private PerkaApplicationResponse PostApplicationToPerka(PerkaApplicationRequest applicationRequest)
+        {
+            return _perkaActions.PostApplication(applicationRequest).Result;
+        }
+    }
+}
--- Perka.Apply.Client/Perka.Apply.Client/Program.cs
+++ Perka.Apply.Client/Perka.Apply.Client/Program.cs
@@ -1,59 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Perka.Apply.Client.Actions;
-using Perka.Apply.Client.Adapters;
-using Perka.Apply.Client.Models;
 
 namespace Perka.Apply.Client
 {
-    internal class Program
+    internal static class Program
     {
+        private static readonly IApplicationActions ApplicationActions = new ApplicationActions();
+
         private static void Main(string[] args)
         {
-            try
-            {
-                var result = GetGithubProjects();
-
-                var resumeContents = GetEncryptedResume();
-
-                var applicationRequest = new PerkaApplicationRequest
-                {
-                    FirstName = "Jonathan",
-                    LastName = "Madden",
-                    Email = "virtualmadden@gmail.com",
-                    PositionId = "GENERALIST",
-                    Explanation = "Custom C# Leveraging HTTPClient - https://github.com/virtualmadden/Perka.Apply.Client",
-                    Projects = result.Select(x => $"{x.Name} - {x.Uri}").ToList(),
-                    Source = "Perka.com",
-                    Resume = resumeContents
-                };
-
-                var response = PostApplicationToPerka(applicationRequest);
-
-                if (response.Response.Equals("You did it!"))
-                    Console.WriteLine(response.Response);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
-
-        private static IEnumerable<GithubProjectResponse> GetGithubProjects()
-        {
-            return new GithubActions().GetProjects().Result;
-        }
-
-        private static string GetEncryptedResume()
-        {
-            return new FileSystemAdapter().GetBase64EncodedResume();
-        }
-
-        private static PerkaApplicationResponse PostApplicationToPerka(PerkaApplicationRequest applicationRequest)
-        {
-            return new PerkaActions().PostApplication(applicationRequest).Result;
+            Console.WriteLine(ApplicationActions.Apply() ? "Success!" : "Failure!");
         }
     }
 }
