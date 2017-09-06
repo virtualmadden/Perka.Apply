596c7cb2af31286772522aec4ce0ce486c53739b
Jonathan Madden <virtualmadden@gmail.com>
2017-09-06 01:32:20 -0700

    Adding some more tests.

---
+++ Perka.Apply.Client/UnitTests/Perka.Apply.Client.UnitTests/Actions/PerkaActionTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Perka.Apply.Client.Actions;
+using Perka.Apply.Client.Adapters;
+using Perka.Apply.Client.Models;
+
+namespace Perka.Apply.Client.UnitTests.Actions
+{
+    [TestFixture]
+    public class PerkaActionTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _perkaApiAdapterMock = new Mock<IPerkaApiAdapter>();
+
+            _perkaApiAdapterMock.Setup(x => x.PostApplicationAsync(It.IsAny<string>())).ReturnsAsync(PerkaResponse);
+
+            _perkaActions = new PerkaActions(_perkaApiAdapterMock.Object);
+        }
+
+        private IPerkaActions _perkaActions;
+        private Mock<IPerkaApiAdapter> _perkaApiAdapterMock;
+        private const string PerkaResponse = "{\"response\": \"Test\"}";
+
+        [Test]
+        public void ShouldReturnASuccessfulResponse()
+        {
+            var result = _perkaActions.PostApplication(new PerkaApplicationRequest()).GetAwaiter().GetResult();
+
+            result.Should().NotBeNull();
+            result.Response.Equals("Test");
+        }
+    }
+}
